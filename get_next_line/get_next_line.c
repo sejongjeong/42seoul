43a79af809b1e27be4ca12e0a362a03adf388495
sejongjeong <wjdtpwhd@gmail.com>
2022-01-06 15:28:24 +0900

    feat: Complete original

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sejeong <sejeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 20:06:57 by sejeong           #+#    #+#             */
-/*   Updated: 2022/01/06 12:57:42 by sejeong          ###   ########.fr       */
+/*   Updated: 2022/01/06 15:27:16 by sejeong          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,31 @@ char	*get_line(char *str)
 	return (line);
 }
 
+char	*free_up(char *str)
+{
+	char	*new_str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (0);
+	}
+	new_str = malloc((ft_strlen(str) - i + 1) * sizeof(char));
+	if (!new_str)
+		return (0);
+	i++;
+	while (str[i])
+		new_str[j++] = str[i++];
+	new_str[j] = '\0';
+	free(left_str);
+	return (new_str);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*str;
@@ -70,9 +95,7 @@ char	*get_next_line(int fd)
 	str = read_from_buffer(fd, str);
 	if (!str)
 		return (0);
-	while (true)
-	{
-		
-	}
-	
+	line = get_line(str);
+	str = free_up(str);
+	return (line);
 }
