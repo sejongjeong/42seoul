32b76ddacfce4fa54ab239a56d04ccf259092b9d
sejongjeong <wjdtpwhd@gmail.com>
2022-01-06 13:06:16 +0900

    feat: Implement original

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,17 +6,70 @@
 /*   By: sejeong <sejeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 20:06:57 by sejeong           #+#    #+#             */
-/*   Updated: 2021/07/31 20:49:04 by sejeong          ###   ########seoul.kr  */
+/*   Updated: 2022/01/06 12:57:42 by sejeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include <unistd.h>
-#include <stdlib.h>
+
+char	*read_from_buffer(int fd, char *str)
+{
+	char	*buffer;
+	size_t	num_read;
+
+	num_read = -1;
+	buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
+	if (!buffer)
+		return (0);
+	while (!ft_strchr(str, '\n') && num_read != 0)
+	{
+		num_read = read(fd, buffer, BUFFER_SIZE);
+		if (num_read == -1)
+		{
+			free(buffer);
+			return (0);
+		}
+		buffer[num_read] = '\0';
+		str = ft_strjoin(str, buffer);
+	}
+	free(buffer);
+	return (str);
+}
+
+char	*get_line(char *str)
+{
+	size_t	i;
+	char	*line;
+
+	i = 0;
+	if (!str[i])
+		return (0);
+	while (str[i] && str[i] != '\n')
+		i++;
+	line = malloc(i * sizeof(char) + 2);
+	if (!line)
+		return (0);
+	ft_memcpy(line, str, sizeof(char) * i);
+	if (str[i] == '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
-	char	buf[4096];
+	static char	*str;
+	char		*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = read_from_buffer(fd, str);
+	if (!str)
+		return (0);
 	while (true)
 	{
 		
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,21 @@
 /*   By: sejeong <sejeong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 20:12:34 by sejeong           #+#    #+#             */
-/*   Updated: 2021/07/31 20:12:35 by sejeong          ###   ########seoul.kr  */
+/*   Updated: 2022/01/06 13:04:50 by sejeong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *dest, const void *src, size_t count);
+char	*get_line(char *str);
+char	*read_from_buffer(int fd, char *str);
+char	*get_next_line(int fd);
+
+#endif
